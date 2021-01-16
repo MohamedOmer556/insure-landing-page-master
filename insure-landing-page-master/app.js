244e2c675b5ae6b66ceb4b2542d55cd5e46a4a4e
MohamedOmer556 <75047921+MohamedOmer556@users.noreply.github.com>
2021-01-16 13:21:16 +0200

    Add files via upload

---
+++ insure-landing-page-master/app.js
@@ -0,0 +1,28 @@
+const burger = document.querySelector('.burger');
+
+
+
+function navToggle(e){
+    if (!e.target.classList.contains('active')){
+        e.target.classList.add('active');
+        gsap.to('.span1',0.4, {rotate: '45', y: '8px', background: 'white'});
+        gsap.to('.span3',0.4, {rotate: '-45', y: '-8px', background: 'white'});
+        gsap.to('.span2', {opacity: 0});
+        gsap.to('.nav-bar',0.5 , {clipPath: 'circle(2500px at 100% -10%)'});
+        document.body.classList.add('hide');
+        
+    }
+    else{
+        e.target.classList.remove('active');
+        gsap.to('.span1',0.4, {rotate: '0', y: '0px', background: 'black'});
+        gsap.to('.span3',0.4, {rotate: '0', y: '-0px', background: 'black'});
+        gsap.to('.span2', {opacity: 1});
+        gsap.to('.nav-bar',0.5 , {clipPath: 'circle(50px at 100% -10%)'});
+        document.body.classList.remove('hide');
+        
+    }
+   
+}
+
+
+burger.addEventListener('click', navToggle);
